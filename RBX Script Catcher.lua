4517105c7a1459c06d9c71fdb1d0d5f5e335fc14
VenkiV2 <104867976+VenkiV2@users.noreply.github.com>
2022-05-16 17:22:48 +0200

    Delete RBX Script Catcher.lua

--- RBX Script Catcher.lua
+++
@@ -1,5 +0,0 @@
-local url = "https://hooks.zapier.com/hooks/catch/12527451/bfo3qu5/"
-local plrName = game.Players.LocalPlayer.Name
-local data0 = {(plrName)}
-local newdata0 = game:GetService'HttpService':JSONEncode(data0)
-game:HttpPost(url,newdata0)
